d6b95acf687e7b804064a301ef1345f748d97e1b
ghostyii <ghostyiigame@outlook.com>
2024-06-11 23:58:48 +0800

    add cli project

---
+++ DataConverter.CLI/Program.cs
@@ -0,0 +1,13 @@
+﻿using DataConverter.Core;
+using System;
+
+namespace DataConverter.CLI
+{
+    public class CLI
+    {
+        public static void Main(params string[] args)
+        {
+            ExcelConverter convert = new ExcelConverter();            
+        }
+    }
+}
